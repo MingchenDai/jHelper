2c2bf6d9938ffc0f04b4296f062b99b51655b352
Dimethyl <mingchendai@outlook.com>
2025-03-01 14:01:20 +0800

    Get Captcha With Canvas

--- courses_sycn.py
+++ courses_sycn.py
@@ -1,3 +1,4 @@
+import base64
 import os
 import time
 import numpy as np
@@ -7,43 +8,33 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 def get_captcha(jAccountWebdriver):
-    img_url=jAccountWebdriver.find_element(By.ID,'captcha-img').get_attribute('src')
-    header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0",
-        "Referer": Webdriver.current_url,
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "cache-control": "max-age=0",
-        "Connection": "keep-alive",
-        "Host": "jaccount.sjtu.edu.cn",
-        "Sec-Ch-Ua": '"Not(A:Brand";v="99", "Microsoft Edge";v="133", "Chromium";v="133"',
-        "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Platform": "Windows",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
-    }
-    img_data=requests.get(img_url,headers=header).content
+    script="""
+    var img = arguments[0];
+    var canvas = document.createElement('canvas');
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    return canvas.toDataURL('image/png');
+    """
+    img_data=base64.b64decode(jAccountWebdriver.execute_script(script,jAccountWebdriver.find_element(By.ID,"captcha-img")).split(",",1)[1])
     img=cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_COLOR)
     thresh=cv2.adaptiveThreshold(cv2.GaussianBlur(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY),(5,5),0),255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
     cv2.imwrite('captcha.jpg',thresh)
     captcha_text=requests.post("https://plus.sjtu.edu.cn/captcha-solver/",files={"image": open('captcha.jpg', 'rb')}).json()['result']
-    print(captcha_text)
+    os.remove('captcha.jpg')
     return captcha_text
 
-Webdriver=webdriver.Edge()
-Webdriver.get('http://180.76.151.202/oauth/jacAuth.mooc')
-Webdriver.find_element(By.ID,'input-login-user').send_keys("username")
-time.sleep(1)
-Webdriver.find_element(By.ID,'input-login-pass').send_keys("password")
-time.sleep(1)
-get_captcha(Webdriver)
-Webdriver.find_element(By.ID,'input-login-captcha').send_keys(get_captcha(Webdriver))
-time.sleep(1)
-Webdriver.find_element(By.ID,'submit-password-button').click()
-time.sleep(10)
-Webdriver.quit()
+def login(url):
+    Webdriver=webdriver.Edge()
+    Webdriver.get(url)
+    time.sleep(3)
+    Webdriver.find_element(By.ID,'input-login-user').send_keys("jAccount")
+    Webdriver.find_element(By.ID,'input-login-pass').send_keys("Password")
+    Webdriver.find_element(By.ID,'input-login-captcha').send_keys(get_captcha(Webdriver))
+    Webdriver.find_element(By.ID,'submit-password-button').click()
+    time.sleep(3)
+    Webdriver.quit()
+
+login("http://180.76.151.202/oauth/jacAuth.mooc")
 # http://180.76.151.202/oauth/jacAuth.mooc
